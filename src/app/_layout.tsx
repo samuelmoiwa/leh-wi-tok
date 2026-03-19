57cb441795a541d156d7c8404f8be384c49cb94b
Moiwa <samuelmoiwa@gmail.com>
2026-03-19 18:58:16 +0000

    fix(update): add toaster to layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,18 @@
 import { Stack } from "expo-router";
 import { PaperProvider } from "react-native-paper";
+import Toast from 'react-native-toast-message';
 
 export default function RootLayout() {
   return (
-    <PaperProvider>
-      <Stack screenOptions={{ headerShown: false }} />
+    <>
+<PaperProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="login" />
+        <Stack.Screen name="signup" />
+      </Stack>
     </PaperProvider>
+    <Toast />
+    </>
+
   );
 }
