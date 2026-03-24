2530e61c36284b7a5d665e01e45b2a0bb46816a4
Moiwa <samuelmoiwa@gmail.com>
2026-03-24 00:40:23 +0000

    fix(update): update layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,8 @@ import { Stack } from "expo-router";
 import { PaperProvider } from "react-native-paper";
 import Toast from "react-native-toast-message";
 import { ThemeProvider, useAppTheme } from "../context/ThemeContext";
+import { useEffect, useState } from "react";
+import { initDatabase } from "../utils/db";
 
 function AppContent() {
   const { theme, isDarkMode } = useAppTheme();
@@ -21,17 +23,34 @@ function AppContent() {
         <Stack.Screen name="home" options={{ headerShown: false }} />
         <Stack.Screen name="settings" options={{ headerShown: true, title: "Settings" }} />
         <Stack.Screen name="edit-profile" options={{ headerShown: true, title: "Edit Profile" }} />
-        <Stack.Screen name="lessons" options={{ headerShown: true, title: "Lessons", }}/>
+        <Stack.Screen name="lessons" options={{ headerShown: true, title: "Lessons" }} />
         <Stack.Screen name="dictionary" options={{ headerShown: true, title: "Dictionary" }} />
         <Stack.Screen name="tok" options={{ headerShown: true, title: "Tok - Translator" }} />
+        <Stack.Screen name="progress" options={{ headerShown: true, title: "Progress Tracking" }} />
       </Stack>
-      {/* Toast is outside the stack to stay on top */}
+
       <Toast />
     </PaperProvider>
   );
 }
 
 export default function RootLayout() {
+  const [isDbReady, setIsDbReady] = useState(false);
+
+  useEffect(() => {
+    try {
+      initDatabase();
+      setIsDbReady(true);
+    } catch (error) {
+      console.log("Database init error:", error);
+    }
+  }, []);
+
+  // 🔥 Prevent app from rendering before DB is ready
+  if (!isDbReady) {
+    return null; // or splash screen / loader
+  }
+
   return (
     <ThemeProvider>
       <AppContent />
